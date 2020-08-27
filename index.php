55922318fd5b0a57465bff4d707e4d57944b536e
ALU-Syntax <ardianiqbal40@gmail.com>
2020-08-28 03:22:02 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <h1>Helo World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, reiciendis! Esse tempora eos animi magni non ea maiores? Voluptatem consectetur dolores quaerat, esse architecto veniam porro tenetur quia voluptatibus? Possimus?</p>
     
 </body>
 </html>
